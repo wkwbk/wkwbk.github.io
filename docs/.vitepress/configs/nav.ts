60c0f94511579efcc759c23c2f10272d59e7fded
wkwbk <lisir.me@outlook.com>
2025-03-24 23:27:51 +0800

    📃 docs(docs\GFW): 科学上网笔记更新

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -55,7 +55,7 @@ export const nav: DefaultTheme.Config['nav'] = [
     text: '科学上网',
     items: [
       { text: '介绍', link: '/GFW/介绍/00.科学上网工具介绍' },
-      { text: '搭建', link: '/GFW/搭建/00.233-一键安装脚本' },
+      { text: '搭建', link: '/GFW/搭建/00.节点搭建教程' },
       { text: '使用', link: '/GFW/使用/00.v2rayN-使用教程' }
     ]
   },
